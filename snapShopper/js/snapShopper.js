990f6700fac9578544a566a4c2f0be3953f595e9
Jonathan Ely <ely.jonathan@gmail.com>
2016-11-23 19:52:18 -0500

    First KO Refactor Branch Commit
    Got it working well enough. Much remains to be done.

--- snapShopper/js/snapShopper.js
+++ snapShopper/js/snapShopper.js
@@ -2,10 +2,35 @@
 var snapShopper = (function () {
   // Properties
   ///////////////////////////
+  var Item = function(data) {
+      this.name = ko.observable();
+      this.price = ko.observable();
+      this.calories = ko.observable();
+      //populate our model with the initial data
+      this.update(data);
+  };
+  Item.prototype.update = function(data) {
+      this.name(data.name || "new item");
+      this.price(data.price || 0);
+      this.calories(data.calories || 800);
+  };
   // Private Methods
   //////////////////////////
-  var myViewModel = function (){// the view model needs to be a function so that computed observables can work
+  var myViewModel = function (items){// the view model needs to be a function so that computed observables can work
     //DATA ---------------------------------------------- //
+    this.items = ko.observableArray(ko.utils.arrayMap(items, function(data) {
+      return new Item(data);
+    }));
+
+    //hold the currently selected item
+    this.selectedItem = ko.observable();
+
+    //make edits to a copy
+    this.itemForEditing = ko.observable(null);
+
+    this.selectItem = this.selectItem.bind(this);
+    this.acceptItem = this.acceptItem.bind(this);
+    this.revertItem = this.revertItem.bind(this);
     this.budgetCap = ko.observable(28);
     this.isGroceryShopping = ko.observable(false);
     this.isAddingFood = ko.observable(false);
@@ -49,9 +74,9 @@ var snapShopper = (function () {
     this.caloriesRemaining = ko.computed(function() {
       return this.caloriesMax() - this.totalCalories();
     }, this);
-    this.moneyLeft = ko.computed(function(){
-      return this.budgetCap() - this.shoppingTotal();
-    });
+    // this.moneyLeft = ko.computed(function(){
+    //   return this.budgetCap() - this.shoppingTotal();
+    // });
     this.setCalories = function(value) {
       this.totalCalories(this.totalCalories()+value);
     };
@@ -62,15 +87,50 @@ var snapShopper = (function () {
       if(confirm("This will reset your grocery list. Are you sure? (Y/N)")) {
         this.totalCalories(0);
         this.shoppingTotal(0);
-        this.foodItems([]);
+        // this.foodItems([]);
+        this.items.removeAll();
       }
     };
   };
+  //TODO: research extend
+  ko.utils.extend(myViewModel.prototype, {
+      //select an item and make a copy of it for editing
+      selectItem: function(item) {
+          //'this' is myViewModel object
+          this.selectedItem(item);
+          this.itemForEditing(new Item(ko.toJS(item)));//convert the item to a plain JS object
+      },
+      // When a user accepts the data, we need to make sure that we update the
+      // cached data with the current state of the model
+      acceptItem: function(item) {
+          var selected = this.selectedItem(),
+           //clean copy of edited converted back to a plain JS object
+              edited = ko.toJS(this.itemForEditing());
+
+          //apply updates from the edited item to the selected item
+          selected.update(edited);
+
+          //clear selected item
+          this.selectedItem(null);
+          this.itemForEditing(null);
+      },
+      //just throw away the edited item and clear the selected observables
+      revertItem: function() {
+          this.selectedItem(null);
+          this.itemForEditing(null);
+      }
+  });
 
   // Public Methods, must be exposed in return statement below
   ///////////////////////////
-   var initKo = function (){
-     ko.applyBindings(myViewModel);
+  var initKo = function (){
+   ko.applyBindings(new myViewModel(
+       [
+         { name: "Cheese", price: 2.50, calories: 100 },
+         { name: "Pepperoni", price: 3.25, calories: 100 },
+         { name: "Deluxe", price: 4.25, calories: 100 }
+       ]
+    ));
    };
 
   // Init
