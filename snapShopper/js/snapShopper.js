b5606b897c407e9d00ca591fc1ab4b11040cb574
Jonathan Ely <ely.jonathan@gmail.com>
2016-11-28 13:24:52 -0500

    CRUD Ops using refactored/abstracted KO
    Also adds some debugging utility

--- snapShopper/js/snapShopper.js
+++ snapShopper/js/snapShopper.js
@@ -10,67 +10,52 @@ var snapShopper = (function () {
       this.update(data);
   };
   Item.prototype.update = function(data) {
-      this.name(data.name || "new item");
-      this.price(data.price || 0);
-      this.calories(data.calories || 800);
+    if(data){
+      this.name(data.name);
+      this.price(data.price);
+      this.calories(data.calories);
+    } else {
+      this.name("");
+      this.price();
+      this.calories();
+    }
   };
   // Private Methods
   //////////////////////////
   var myViewModel = function (items){// the view model needs to be a function so that computed observables can work
+    var self = this;
     //DATA ---------------------------------------------- //
     this.items = ko.observableArray(ko.utils.arrayMap(items, function(data) {
       return new Item(data);
     }));
-
     //hold the currently selected item
     this.selectedItem = ko.observable();
-
     //make edits to a copy
     this.itemForEditing = ko.observable(null);
-
+    //make this level of the view model available to the Item object context
     this.selectItem = this.selectItem.bind(this);
     this.acceptItem = this.acceptItem.bind(this);
     this.revertItem = this.revertItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    ////////////////////////////////////////////////////
     this.budgetCap = ko.observable(28);
     this.isGroceryShopping = ko.observable(false);
     this.isAddingFood = ko.observable(false);
     this.shoppingTotal =ko.observable(0);
     this.caloriesMax = ko.observable(2400);
     this.totalCalories = ko.observable(0);
-    this.foodItem = {
-      itemName: ko.observable('Potatoes'),
-      itemCost: ko.observable(1.50),
-      itemCals: ko.observable(800),
-      itemCalsPerDollar: ko.computed(function(){}),
-      itemPercentBudget: ko.computed(function(){})
-    };
-    this.foodItems = ko.observableArray([
-      {itemName: 'Potatoes', itemCost: 1.50, itemCals: 800}
-    ]);
     //METHODS ------------------------------------------ //
-    this.addFoodItem = function(item) {
-      var newItem = {
-        itemName: item.itemName(),
-        itemCost: item.itemCost(),
-        itemCals: item.itemCals()
-      };
-      this.foodItems.push(newItem);
-      this.shoppingTotal(this.shoppingTotal()+item.itemCost());
-      this.totalCalories(this.totalCalories()+item.itemCals());
-      isAddingFood(false);
-    };
-    this.editFoodItem = function (){
-      isAddingFood(true);
-      foodItem.itemName(this.itemName);
-      foodItem.itemCost(this.itemCost);
-      foodItem.itemCals(this.itemCals);
-      console.log(this);
-      console.log(ko.toJSON(foodItem));
-    };
-    this.removeItem = function () {
-      console.log(this);
-      foodItems.remove(this);
-    };
+    // this.addFoodItem = function(item) {
+    //   var newItem = {
+    //     itemName: item.itemName(),
+    //     itemCost: item.itemCost(),
+    //     itemCals: item.itemCals()
+    //   };
+    //   this.foodItems.push(newItem);
+    //   this.shoppingTotal(this.shoppingTotal()+item.itemCost());
+    //   this.totalCalories(this.totalCalories()+item.itemCals());
+    //   isAddingFood(false);
+    // };
     this.caloriesRemaining = ko.computed(function() {
       return this.caloriesMax() - this.totalCalories();
     }, this);
@@ -91,33 +76,69 @@ var snapShopper = (function () {
         this.items.removeAll();
       }
     };
+    this.formatCurrency = function(value) {
+      return "$" + value().toFixed(2);
+    };
+    // this.removeItem = function (item) {
+    //   self.items.remove(item);
+    // };
   };
   //TODO: research extend
   ko.utils.extend(myViewModel.prototype, {
       //select an item and make a copy of it for editing
       selectItem: function(item) {
-          //'this' is myViewModel object
-          this.selectedItem(item);
-          this.itemForEditing(new Item(ko.toJS(item)));//convert the item to a plain JS object
+        debugger;
+        //'this' is myViewModel object
+        this.selectedItem(item);
+        this.itemForEditing(new Item(ko.toJS(item)));//convert the item to a plain JS object
+        console.log(this.items());
+        console.log(this.itemForEditing());
       },
       // When a user accepts the data, we need to make sure that we update the
       // cached data with the current state of the model
       acceptItem: function(item) {
-          var selected = this.selectedItem(),
-           //clean copy of edited converted back to a plain JS object
-              edited = ko.toJS(this.itemForEditing());
+        var selected = this.selectedItem(),
+         //clean copy of edited converted back to a plain JS object
+            edited = ko.toJS(this.itemForEditing());
 
-          //apply updates from the edited item to the selected item
-          selected.update(edited);
+        //apply updates from the edited item to the selected item
+        selected.update(edited);
 
-          //clear selected item
-          this.selectedItem(null);
-          this.itemForEditing(null);
+        //clear selected item
+        this.selectedItem(null);
+        this.itemForEditing(null);
+        console.log(this.items());
       },
       //just throw away the edited item and clear the selected observables
       revertItem: function() {
-          this.selectedItem(null);
-          this.itemForEditing(null);
+        //TODO: delete newly created items from the items collection
+        debugger;
+        this.selectedItem(null);
+        this.itemForEditing(null);
+        console.log(this.items());
+        console.log(this.selectedItem());
+        console.log(this.itemForEditing());
+      },
+      createItem: function () {
+        var newItem = new Item();
+        newItem.update();
+        this.items.push(newItem);
+        this.selectItem(newItem);
+      },
+      removeItem: function(item) {
+        //NOTE: deliberately using double equals to check for null and undefined
+        //see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
+        //if we haven't opened an individual item...
+        if(this.selectedItem() == null) {
+          //...delete the item object reference being passed in
+          this.items.remove(item);
+        }
+        //we've selected and opened an item...
+        else {
+          //...so delete that selected item object reference
+          this.items.remove(this.selectedItem());
+          this.revertItem();
+        }
       }
   });
 
@@ -132,9 +153,6 @@ var snapShopper = (function () {
        ]
     ));
    };
-
-  // Init
-  ///////////////////////////
 
   // Reveal public methods
   return {
