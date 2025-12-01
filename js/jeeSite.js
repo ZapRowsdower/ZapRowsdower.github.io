12d0d1f5701491b6d8f84122730b8af540a4cd24
ZapRowsdower <ZapRowsdower@users.noreply.github.com>
2025-11-30 19:38:14 -0500

    - Fixes nanogallery not displaying consistently - Portfolio section uses tabs - use css vars over sass - rm unused contact form

--- js/jeeSite.js
+++ js/jeeSite.js
@@ -1,47 +1,39 @@
-/// <reference types="./JQueryStatic.d.ts" />
+/// <reference types="./globals.d.ts" />
 
 (function () {
 
 	try {
-		/**
-		 * 
-		 * @param {string} galleryElem 
-		 */
-		function flickrNano(galleryElem) {
-			//@ts-expect-error
-			$(galleryElem).nanoGallery({
-				kind: 'flickr',
-				userID: '142772868@N08',
-				photoset: '72157667342772360',
-				thumbnailWidth: 'auto',
-				thumbnailHeight: 250,
-				thumbnailHoverEffect: [{ name: 'labelAppear75', duration: 300 }],
-				thumbnailLazyLoad: true,
-				theme: 'light',
-				viewerToolbar: { style: 'fullWidth' },
-				thumbnailGutterWidth: 0,
-				thumbnailGutterHeight: 0,
-				fnInitGallery: function () {
-					$(galleryElem).hide();
-				}
-			});
-		};
 
-		flickrNano('#photoPortfolio');
-
-		$('.navbar-fixed-top a, .back-to-top-wrapper a, .portfolio-nav').bind('click', function (event) {
-			//portfolio nav behavior for showing/hiding selected sections
-			if ($(this).hasClass("portfolio-nav")) {
-				$(".portfolio-section").hide();
-				$($(this).attr("href")).show();
-			}
+		$('#photoPortfolio').nanoGallery({
+			kind: 'flickr',
+			userID: '142772868@N08',
+			photoset: '72157667342772360',
+			thumbnailWidth: 'auto',
+			thumbnailHeight: 250,
+			thumbnailHoverEffect: [{ name: 'labelAppear75', duration: 300 }],
+			thumbnailLazyLoad: true,
+			theme: 'light',
+			viewerToolbar: { style: 'fullWidth' },
+			thumbnailGutterWidth: 0,
+			thumbnailGutterHeight: 0
 		});
 
-		//init bootstrap scrollspy for nav section highlighting behavior
-		//@ts-expect-error
+		// init bootstrap scrollspy for nav section highlighting behavior
 		$('body').scrollspy({
 			target: '.navbar',
 			offset: 160
+		});
+
+		// Make sure gallery displays properly when this tab is open
+		/** @type {HTMLDetailsElement | null} */
+		const gallery_tab = document.querySelector('.nanoGallery');
+		gallery_tab?.addEventListener("toggle", (e) => {
+			if (gallery_tab.hasAttribute('open')) {
+				// we need to reload if the thumbnail widths are 0px
+				if(gallery_tab.querySelector('.nanoGalleryThumbnailContainer')?.getAttribute('style')?.includes('width: 0px')) {
+					$('#photoPortfolio').nanoGallery('reload');
+				}
+			}
 		});
 
 	} catch (error) {
